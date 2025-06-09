31bfce8a462f42ae40430f5e011bc679d21fa6ca
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-06-09 18:12:03 +0200

    fix: Snackbar should be placed above FAB
    Closes: #43

--- app/src/main/java/fr/husta/android/dark_theme_tester/MainActivity.kt
+++ app/src/main/java/fr/husta/android/dark_theme_tester/MainActivity.kt
@@ -60,7 +60,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+                .setAnchorView(R.id.fab)
+                .setAction("Action", null)
+                .show()
         }
     }
 
